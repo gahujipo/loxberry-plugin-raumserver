13e66a9369b0635fe95fa22c09867be73acef097
gahujipo <26710574+gahujipo@users.noreply.github.com>
2020-03-20 22:46:47 +0100

    introducing compatibility for LoxBerry 2.x (#6)
    * move node installation to preroot
    
    * these changes need the v2 plugin interface
    
    * move the raumserver installation to postinstall.sh - cleanup daemon
    
    * fix preRoot by exiting 0 when everything is already installed
    
    * fix warnings during installation

--- postinstall.sh
+++ postinstall.sh
@@ -20,8 +20,51 @@
 # <ERROR> This is an error!"
 # <FAIL> This is a fail!"
 
-echo "<WARN> You need to reboot Loxberry in order to complete the installation"
-echo "<WARN> Loxberry muss neu gestartet werden um die Installation abzuschliessen."
+# To use important variables from command line use the following code:
+COMMAND=$0    # Zero argument is shell command
+PTEMPDIR=$1   # First argument is temp folder during install
+PSHNAME=$2    # Second argument is Plugin-Name for scipts etc.
+PDIR=$3       # Third argument is Plugin installation folder
+PVERSION=$4   # Forth argument is Plugin version
+#LBHOMEDIR=$5 # Comes from /etc/environment now. Fifth argument is
+              # Base folder of LoxBerry
+
+# Combine them with /etc/environment
+PCGI=$LBPCGI/$PDIR
+PHTML=$LBPHTML/$PDIR
+PTEMPL=$LBPTEMPL/$PDIR
+PDATA=$LBPDATA/$PDIR
+PLOG=$LBPLOG/$PDIR # Note! This is stored on a Ramdisk now!
+PCONFIG=$LBPCONFIG/$PDIR
+PSBIN=$LBPSBIN/$PDIR
+PBIN=$LBPBIN/$PDIR
+
+if [ -d $PDATA ]
+then 
+    # change to raumserver folder
+    cd $PDATA
+    
+    #check if raumserver already installed
+    if [ `npm list | grep -c "node-raumserver"` -ne 1 ]
+    then
+        /usr/bin/logger "Raumserver installation: node module raumserver not instlled. Starting installation now."
+        # otherwise install the server
+        npm install github:ChriD/node-raumserver
+    fi
+
+    /usr/bin/logger "Raumserver: starting."
+    #run the server: 
+    cd $PDATA/node_modules/node-raumserver/
+    node raumserver.js &
+
+    if [ `ps ax | grep -v grep | grep -c raumserver.js` -eq 1 ]
+    then
+        /usr/bin/logger "Raumserver: Start successful."
+    else
+        /usr/bin/logger "Raumserver: Start failed."
+    fi
+fi
+
 
 # Exit with Status 0
 exit 0
